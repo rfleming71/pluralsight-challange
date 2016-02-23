ba086b9c5301d77d675253edc572cb8896e54fc8
Ryan Fleming <rfleming71@gmail.com>
2016-02-23 09:32:55 -0500

    Adding paging to the question index

--- pluralsight-challenge/Scripts/AllQuestionsView.js
+++ pluralsight-challenge/Scripts/AllQuestionsView.js
@@ -1,41 +1,41 @@
-﻿var AllQuestionsView = Backbone.View.extend({
-    events: {
-        "click tbody tr": "questionRowClick"
-    },
-    initialize: function (options) {
-        this.collection = new QuestionCollection({ url: options.url });
-        this.listenTo(this.collection, "change", this.render);
-        this.listenTo(this.collection, "sort", this.render);
-        this.listenTo(this.collection, "update", this.render);
-    },
-    render: function () {
-        var totalHtml = "";
-        _.each(this.collection.models, function (model) {
-            var htmlRow = "<tr data-question-id='" + model.get("QuestionId") + "'><td>" + model.get("Text") + "</td><td>" + model.get("Answer") + "</td><td>";
-            _.each(model.get("Distractors"), function (value, key) {
-                if (key != 0) {
-                    htmlRow += ", ";
-                }
+﻿var AllQuestionsView = function (options) {
+    var data = {};
+    data.el = options.el;
+    data.table = data.el.find("#questionTable");
+    data.table.jqGrid({
+        url: options.url,
+        datatype: "json",
+        height: "800px",
+        colNames: ["Text", "Answer", "Distractors", "QuestionId"],
+        colModel: [
+            { name: "Text", index: "Text", searchoptions: { sopt: ["eq", "cn", "bw"] } },
+            { name: "Answer", index: "Answer", searchoptions: { sopt: ["eq", "cn", "bw"] } },
+            { name: "Distractors", index: "Distractors", searchoptions: { sopt: ["eq", "cn", "bw"] } },
+            { name: "QuestionId", index: "QuestionId", hidden: true },
+        ],
+        rowNum: 30,
+        rowList: [30, 50, 100],
+        pager: "#pager",
+        viewrecords: true,
+        sortorder: "asc",
+        autowidth: true,
+        loadonce: true,
+        onSelectRow: function (id) {
+            var rowData = $(this).getLocalRow(id);
+            window.location = "#question/" + rowData.QuestionId;
+        }
+    });
+    
+    data.table.jqGrid("filterToolbar", { searchOperators: true });
 
-                htmlRow += value;
-            });
 
-            htmlRow += "</td></tr>";
-            totalHtml += htmlRow;
-        });
-
-        this.$el.find("tbody").html(totalHtml);
-    },
-    questionRowClick: function (ev) {
-        var target = $(ev.target).closest("tr");
-        var questionId = target.attr("data-question-id");
-        window.location = "#question/" + questionId;
-    },
-    show: function () {
-        this.collection.fetch();
-        this.$el.show();
-    },
-    hide: function () {
-        this.$el.hide();
-    }
-});
+    return {
+        show: function() {
+            data.table.setGridParam({ datatype: "json", page: 1 }).trigger('reloadGrid');
+            data.el.show();
+        },
+        hide: function() {
+            data.el.hide();
+        }
+    };
+};
